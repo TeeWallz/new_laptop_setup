a30d46f86942e00c055ecd7d0f773ae3a5916e95
TeeWallz <thomas.r.waller@gmail.com>
2022-01-04 17:17:01 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,62 @@
 #!/bin/bash
 
+curl -Lks https://gist.githubusercontent.com/TeeWallz/0fc9cbfaeeb5a4a0ce9440446174a5ff/raw/4a1b812008bd342f8ab6bf997e414a8f6626d280/install-dotfiles.sh | /bin/bash
+
 pacman -Syyu
-pacman -S i3-gaps feh dmenu i3status chromium code chromium git dbeaver flameshot vlc steam rofi
+pacman -Syy base-devel i3-gaps feh dmenu i3status code git dbeaver flameshot vlc steam rofi gparted aws-cli
 
-sudo tee -a /usr/share/xsessions/plasma-i3.desktop > /dev/null <<EOT
-[Desktop Entry]
-Type=XSession
-Exec=env KDEWM=/usr/bin/i3 /usr/bin/startplasma-x11
-DesktopNames=KDE
-Name=Plasma with i3
-Comment=Plasma with i3
-EOT
-
-cp .i3_config ~/.config/i3/config
-
-mkdir /usr/share/backgrounds/
-cp wallpaper.png /usr/share/backgrounds/wallpaper.png
-exec --no-startup-id feh --bg-scale /usr/share/backgrounds/background.jpg
+snap install nordpass
 
 cd /opt
 git clone https://aur.archlinux.org/yay-git.git
-chown -R tom:tom ./yay-git
+chown -R tom:tom /opt/yay-git
 cd yay-git
 makepkg -si
 
-yay -S slack-desktop teams teamviewer caprine postman-bin compton-tryone-git oh-my-zsh-git
+yay -S slack-desktop teams teamviewer caprine postman-bin compton-tryone-git oh-my-zsh-git onedrive-abraunegg google-chrome
 
 cd /tmp
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
+git clone https://github.com/DanielBorgesOliveira/onedrive_tray.git
+cd onedrive_tray
+mkdir build
+cd build
+qmake ../systray.pro
+make
+cp onedrive_tray /usr/local/bin/onedrive_tray
+#onedrive_tray --onedrive-path [path to onedrive client] --onedrive-args [onedrive client arguments].
 
+
+# New home?
+#onedrive
+
+
+
+
+
+# i3
+# sudo tee -a /usr/share/xsessions/plasma-i3.desktop > /dev/null <<EOT
+# [Desktop Entry]
+# Type=XSession
+# Exec=env KDEWM=/usr/bin/i3 /usr/bin/startplasma-x11
+# DesktopNames=KDE
+# Name=Plasma with i3
+# Comment=Plasma with i3
+# EOT
+#
+# cp .i3_config ~/.config/i3/config
+#
+# mkdir /usr/share/backgrounds/
+# cp wallpaper.png /usr/share/backgrounds/wallpaper.png
+# exec --no-startup-id feh --bg-scale /usr/share/backgrounds/background.jpg
+
+
+
+# Powerline
+# cd /tmp
+# git clone https://github.com/powerline/fonts.git
+# cd fonts
+# ./install.sh
+
+
+# Natural scroll
+# KDE redshift
